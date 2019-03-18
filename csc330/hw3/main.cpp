b30b959a6bf80921aee5c039d20ab7de405f849b
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-03-17 21:12:27 -0400

    csc330 hw3 completion

--- csc330/hw3/main.cpp
+++ csc330/hw3/main.cpp
@@ -1,38 +1,75 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <map>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-	// get name of file to read
-	string filename;
-	cout << "Enter name of file to read: ";
-	cin >> filename;
+	if(argc == 1)
+	{
+		cout << "Please specify a filename." << endl;
+		return 1;
+	}
 
+	// get filename from argv
+	string filename = argv[1];
+
+	// try to open file
 	fstream infile;
 	infile.open(filename);
 
 	// if file open failure
 	if(!infile.is_open())
 	{
-		cout <<"Could not open file "<<filename<<". Exiting."<< endl;
+		cout <<"Could not open file `"<<filename<<"`. Exiting."<< endl;
 		return 1;
 	}
 
+	cout<<"Reading from `"<<filename<<"`."<<endl;
+
 	// map of words and counts
-	map<string, int> words;	
+	map<string, int> wordmap;	
+
+	// characters to ignore in file
+	string ignore = ".?!,\n";
 
 	// read file and count word appearances
 	// ====================================
 
-	string line; // file line buffer
-	while(getline(infile, line))
+	// tokenize file
+	string tok; // file token buffer
+	while(getline(infile, tok, ' ')) // space delimiter
 	{
-		cout << line << endl;
+		// convert token to lowercase
+		for(int i=0; i < tok.length(); i++)
+			tok[i] = tolower(tok[i]);
+
+		// remove unwanted characters
+		for(auto c : ignore)
+		{
+			if(tok.find(c) != -1)
+				tok.erase(tok.find(c),1);
+		}
+
+		// add token to wordmap or increment count
+		if(wordmap.find(tok) != wordmap.end()) // increment if found
+			wordmap[tok]++;
+		else // not found, insert new word
+			wordmap.insert({tok, 1});
 	}
+
+	// display words and word counts
+	int i = 0;
+	for(auto word : wordmap)
+	{
+		i++;
+		cout <<word.first<<"\t"<<word.second;
+		i%4 == 0 ? cout << endl : cout << "\t";
+	}
+
 
 	infile.close();
 
