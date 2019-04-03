3bbf01a37699dede287ee4f2d08ae77273ec093b
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-04-03 13:48:36 -0400

    csc305 sucessful sort of processes by exec time for sjn

--- csc305/lab3/sjn.cpp
+++ csc305/lab3/sjn.cpp
@@ -1,4 +1,4 @@
-// second job next scheduling
+// shortest job next scheduling
 
 #include <iostream>
 #include <cstdio>
@@ -6,14 +6,10 @@
 using namespace std;
 
 // process struct
-struct proc
-{
+struct proc {
 	// set by user
-	int arrivalTime = 0;
 	int execTime = 0;
-
 	// calculated
-	int serviceTime = 0;
 	int turnaroundTime = 0;
 };
 
@@ -24,9 +20,9 @@ int main()
 	int numProc = 0;
 
 	// get num. of processes
-	while (numProc < 1 || numProc > MAX_PROC)
+	while(numProc < 1 || numProc > MAX_PROC)
 	{
-		cout << "Enter num. of processes (from 1-" << MAX_PROC << "): ";
+		cout << "Enter num. of processes (from 1-"<<MAX_PROC<<"): ";
 		cin >> numProc;
 	}
 
@@ -37,44 +33,63 @@ int main()
 	double avgTurnaroundTime = 0;
 
 	// get arrival times and exec times
-	for (int i = 0; i < numProc; i++)
+	for(int i=0; i < numProc; i++)
 	{
-		cout << "Enter arrival time for proc " << i << ": ";
-		cin >> procArr[i].arrivalTime;
-
 		cout << "Enter exec time for proc " << i << ": ";
 		cin >> procArr[i].execTime;
-
-		// sort processes by exec. time
 	}
 
-	for (int i = 0; i < numProc; i++)
+    // sort in ascending order by exec time (bubble sort)
+	for(int i=0; i < numProc-1; i++)
 	{
-		// calculate service time: sum previous exec times
-		for (int j = 0; j < i; j++)
-			procArr[i].serviceTime += procArr[j].execTime;
+        // temp storage
+        proc smallest = procArr[i];
+        int index = -1; // index
 
-		// calculate turnaround time
-		procArr[i].turnaroundTime = procArr[i].serviceTime - procArr[i].arrivalTime;
-	}
+	    for(int j=i+1; j < numProc; j++)
+        {
+            if(procArr[j].execTime < smallest.execTime)
+            {
+                smallest = procArr[j];
+                index = j;
+            }
+        }
+
+        // if smallest item was found
+        if(index != -1)
+        {
+            procArr[index] = procArr[i];
+            procArr[i] = smallest;
+        }
+    }
+
+/*
+	// calculate turnaround time: sum previous exec times
+	for(int i=0; i < numProc; i++)
+	{
+		for(int j=0; j<i; j++)
+			procArr[i].turnaroundTime += procArr[j].execTime;
+    }
 
 	// calculate avg. turnaround
 	// sum
-	for (int i = 0; i < numProc; i++)
+	for(int i=0; i < numProc; i++)
 		avgTurnaroundTime += procArr[i].turnaroundTime;
 
 	// division
 	avgTurnaroundTime /= numProc;
 
 	// display results
-	cout << "Proc, Arrival T., Exec. T., Service T., Turnaround T." << endl;
-	for (int i = 0; i < numProc; i++)
+	cout << "Proc, Arrival T., Exec. T., Turnaround T." << endl;
+	for(int i=0; i < numProc; i++)
 	{
 		proc process = procArr[i];
 
-		printf("p%d\t%d\t%d\t%d\t%d\n", i, i, process.execTime, process.serviceTime, process.turnaroundTime);
+		printf("p%d\t0\t%d\t%d\n",i,process.execTime, process.turnaroundTime);
 	}
 	cout << "Avg. turnaround time: " << avgTurnaroundTime << endl;
+
+*/
 
 	return 0;
 }
