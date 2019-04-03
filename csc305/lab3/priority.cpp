34530ad16eb67f804078c94397a543888a4a04f1
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-04-03 16:26:44 -0400

    added priority scheduling

--- csc305/lab3/priority.cpp
+++ csc305/lab3/priority.cpp
@@ -9,6 +9,8 @@ using namespace std;
 struct proc {
 	// set by user
 	int execTime = 0;
+    int priority = 0;
+
 	// calculated
 	int turnaroundTime = 0;
 };
@@ -37,29 +39,32 @@ int main()
 	{
 		cout << "Enter exec time for proc " << i << ": ";
 		cin >> procArr[i].execTime;
+
+		cout << "Enter priority for proc " << i << ": ";
+		cin >> procArr[i].priority;
 	}
 
-    // sort in ascending order by exec time (bubble sort)
+    // sort in descending order by priority (bubble sort)
 	for(int i=0; i < numProc-1; i++)
 	{
         // temp storage
-        proc smallest = procArr[i];
+        proc highest = procArr[i];
         int index = -1; // index
 
 	    for(int j=i+1; j < numProc; j++)
         {
-            if(procArr[j].execTime < smallest.execTime)
+            if(procArr[j].priority >= highest.priority)
             {
-                smallest = procArr[j];
+                highest = procArr[j];
                 index = j;
             }
         }
 
-        // if smallest item was found
+        // if highest priority was found
         if(index != -1)
         {
             procArr[index] = procArr[i];
-            procArr[i] = smallest;
+            procArr[i] = highest;
         }
     }
 
@@ -79,12 +84,12 @@ int main()
 	avgTurnaroundTime /= numProc;
 
 	// display results
-	cout << "Proc, Arrival T., Exec. T., Turnaround T." << endl;
+	cout << "Proc, Priority, Arrival T., Exec. T., Turnaround T." << endl;
 	for(int i=0; i < numProc; i++)
 	{
 		proc process = procArr[i];
 
-		printf("p%d\t0\t%d\t%d\n",i,process.execTime, process.turnaroundTime);
+		printf("p%d\t%d\t0\t%d\t%d\n",i,process.priority,process.execTime, process.turnaroundTime);
 	}
 	cout << "Avg. turnaround time: " << avgTurnaroundTime << endl;
 
